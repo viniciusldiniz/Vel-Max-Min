0552204b0f53ad0e1fbdd07ca0d07bb437670e0a
Vinicius L. Diniz <42078940+viniciusldiniz@users.noreply.github.com>
2024-03-26 16:48:30 -0300

    Add files via upload

---
+++ Vel. Max-min.js
@@ -0,0 +1,9 @@
+var vel = 595
+console.log(`A velocidade do seu carro é de ${vel} km/h`)
+if(vel > 60){
+    console.log('Você ultrapassou a velocidade permitida. MULTADO EM 100.000,00 PESOS ARGENTINOS !')
+    console.log('Diminua a velocidade e dirija com  segurança.')
+}   
+else{ console.log('Você está na velocidade permitida, BOA VIAGEM!   ')
+console.log('dirija usando o cinto de segurança')
+}
